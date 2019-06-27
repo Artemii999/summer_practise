14551119549767593055e9d541a996bcd56fe030
Artemii999 <chekanovartemii99@mail.ru>
2019-06-28 00:43:29 +0300

    correction in task1
    Все замечания исправлены

--- Task1/src/input.js
+++ Task1/src/input.js
@@ -1,21 +1,22 @@
-var fullDateTime = new Date(); 
-var getNormalDate = function(date){ 
-var day = date.getDate();
-if (day<10) day = '0' + day; 
-var month = date.getMonth() + 1;
-if (month<10) month = '0' + month; 
-var year = date.getFullYear(); 
-return day + '.' + month + '.' + year; 
-}; 
-document.getElementById('date_real').value = getNormalDate(fullDateTime);
+const fullDateTime = new Date(); 
 
-var fullDateTime = new Date(); 
-var getNormalDate = function(date){ 
-var day = date.getDate() + 1;
-if (day<10) day = '0' + day; 
-var month = date.getMonth() + 1;
-if (month<10) month = '0' + month; 
-var year = date.getFullYear(); 
-return day + '.' + month + '.' + year; 
-}; 
-document.getElementById('date_tomorrow').value = getNormalDate(fullDateTime);
+const getNormalDate = (date) => { 
+    let day = date.getDate();
+    if (day<10) day = '0' + day; 
+    let month = date.getMonth() + 1;
+    if (month<10) month = '0' + month; 
+    const year = date.getFullYear(); 
+    return day + '.' + month + '.' + year; 
+    }; 
+
+const getTomorrowDate = (date) => { 
+    let day = date.getDate() + 1;
+    if (day<10) day = '0' + day; 
+    let month = date.getMonth() + 1;
+    if (month<10) month = '0' + month; 
+    const year = date.getFullYear(); 
+    return day + '.' + month + '.' + year; 
+    }; 
+
+document.getElementById('date_real').value = getNormalDate(fullDateTime);
+document.getElementById('date_tomorrow').value = getTomorrowDate(fullDateTime);
